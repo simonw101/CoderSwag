1441e046e59c5138c05c89b3cedd1bdfa1947245
simon <simonhanwilson@icloud.com>
2019-09-28 19:49:28 +0100

    created custom listview cell

--- app/src/main/java/com/example/coderswag/Modal/Category.kt
+++ app/src/main/java/com/example/coderswag/Modal/Category.kt
@@ -1,3 +1,9 @@
 package com.example.coderswag.Modal
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+
+    override fun toString(): String {
+        return title
+    }
+
+}
