90756bbcf724a5bfdac7108f81cab46cb4e94912
simon <simonhanwilson@icloud.com>
2019-10-01 19:42:01 +0100

    finished products activity with custom products adapter which implements recyclerview

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,11 +1,18 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductsAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +20,30 @@ class ProductsActivity : AppCompatActivity() {
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
 
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        val orientation = resources.configuration.orientation
+
+        var spanCOunt = 2
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            spanCOunt = 3
+
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+
+        if (screenSize > 720) {
+
+            spanCOunt == 3
+
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCOunt)
+
+        productListView.layoutManager = layoutManager
+
+        productListView.adapter = adapter
     }
 }
